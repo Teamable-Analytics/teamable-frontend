d657e422259f81afdf7414f644e43ac37e811506
Abhinav Thota <abhinavthota01@gmail.com>
2024-01-10 10:34:12 -0800

    Initial commit to branch, added new page where data will be imported.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import {Button} from "@/components/ui/button";
+import Link from 'next/link';
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Test button</Button>
+      <Link href='/importData'>Import data</Link>
     </main>
   )
 }
