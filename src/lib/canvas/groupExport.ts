f3d7c002274a8453d437775ed2d9b548f0ef79bc
justino599 <justin.schoenit@gmail.com>
2024-01-13 10:33:07 -0800

    Start adding some error handling

--- src/lib/canvas/groupExport.ts
+++ src/lib/canvas/groupExport.ts
@@ -8,6 +8,17 @@ import {TeamSet} from "@/types/TeamSet"
  * @returns {string} The CSV representation of the TeamSet for Canvas.
  */
 export function exportTeamSetToCanvas(teamSet: TeamSet): string {
+    // Check that all team names are unique
+    const teamNames: Set<string> = new Set()
+    for (const team of teamSet.teams) {
+        const name = team.name || `Group ${team.id}`
+        if (teamNames.has(name)) {
+            // Duplicate team name found
+            // TODO: Decide what to do when there are duplicate team names. Maybe default to all use `Group ${id}` format
+        }
+        teamNames.add(name)
+    }
+
     let exportedCSV = teamSet.teams.map(team => {
         const teamName = team.name || `Group ${team.id}`
         return team.students.map(student => `${student.id},${teamName}`).join("\n")
