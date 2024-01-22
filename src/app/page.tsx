d99dbcc2561992cac4e171d4fc84e0ba3aba42d1
Abhinav Thota <abhinavthota01@gmail.com>
2024-01-22 11:17:05 -0800

    reverted page.tsx back to the same one in main branch.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 import {Button} from "@/components/ui/button"
-import Link from 'next/link'
-
-
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <Link href="/importData">Import data</Link>
+            <Button>Test button</Button>
         </main>
     )
 }
