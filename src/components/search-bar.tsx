f363234eb0616d5432211b955ed4b802d2c7b881
ketphan02 <tuankietvn@outlook.com>
2024-02-06 04:19:01 -0800

    Use radix icon instead of svg

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -1,14 +1,14 @@
 import { Input, type InputProps } from "@/components/ui/input"
 import * as React from "react"
 import { cn } from "@/lib/utils"
-import { Icons } from "@/components/ui/icons"
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons"
 
 interface SearchProps extends InputProps {}
 
 const SearchBar = React.forwardRef<HTMLInputElement, SearchProps>(({className, type, ...props}, ref) => {
     return (
         <div className="flex items-center w-full -ml-4">
-            <Icons.magnifyingGlass className="relative left-6"/>
+            <MagnifyingGlassIcon className="relative left-6"/>
             <Input
                 type={type || "search"}
                 ref={ref}
