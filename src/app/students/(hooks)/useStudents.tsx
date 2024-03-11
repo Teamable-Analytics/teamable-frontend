ff79e9673a9cae505d5a83b552d062f77374cbff
Abhinav Thota <abhinavthota01@gmail.com>
2024-03-11 16:58:01 -0700

    added minor documentation and moved fetchStudents function

--- src/app/students/(hooks)/useStudents.tsx
+++ src/app/students/(hooks)/useStudents.tsx
@@ -17,9 +17,18 @@ const StudentsContext = createContext<StudentsContextType | undefined>(undefined
 const useStudentsProvider = (): StudentsContextType => {
     const [displayStudents, setDisplayStudents] = useState<Student[]>([])
     const [sections, setSections] = useState<DropdownOption[]>([])
+    const fetchStudents = async () => {
+        const fixedCourseNum = 1  // to-do: replace with actual course number
+        const courseMemberData = await fetch(`http://127.0.0.1:8000/api/v1/course-members/?course=${fixedCourseNum}`).then(res => res.json())
+        const studentsToDisplay: Student[] = courseMemberData.results.map((member: any) => ({
+            id: member.id,
+            name: member.user.last_name + ',' + member.user.first_name,
+            sections: member.sections.map((section: any) => section.name),
+        }))
+        setDisplayStudents(studentsToDisplay)
+    }
 
-    const fixedCourseNum = 1
-
+    // to-do: remove this useMemo and use API call to get a set of sections all student course_members that are in specific course
     useMemo(() => {
         const sections = new Set<string>()
         displayStudents.forEach(student => {
@@ -34,15 +43,7 @@ const useStudentsProvider = (): StudentsContextType => {
     return {
         displayStudents: displayStudents,
         currentSections: sections,
-        fetchStudents: async () => {
-            const courseMemberData = await fetch(`http://127.0.0.1:8000/api/v1/course-members/?course=${fixedCourseNum}`).then(res => res.json())
-            const studentsToDisplay: Student[] = courseMemberData.results.map((member: any) => ({
-                id: member.id,
-                name: member.user.last_name + ',' + member.user.first_name,
-                sections: member.sections.map((section: any) => section.name),
-            }))
-            setDisplayStudents(studentsToDisplay)
-        },
+        fetchStudents,
     }
 }
 
