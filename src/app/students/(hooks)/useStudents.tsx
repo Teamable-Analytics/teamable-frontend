9a4b84847f67289b8e76b088dc77ab839695d3f2
Abhinav Thota <abhinavthota01@gmail.com>
2024-03-17 13:15:48 -0700

    use env variable instead

--- src/app/students/(hooks)/useStudents.tsx
+++ src/app/students/(hooks)/useStudents.tsx
@@ -2,15 +2,15 @@
 import { useState, createContext, useContext, PropsWithChildren, useMemo } from 'react'
 import { Student } from '@/_temp_types/student'
 
-type DropdownOption  = {
-  label: string;
-  value: string;
+type DropdownOption = {
+    label: string;
+    value: string;
 }
 
 type StudentsContextType = {
-  displayStudents: Student[];
-  currentSections: DropdownOption[];
-  fetchStudents: () => Promise<void>;
+    displayStudents: Student[];
+    currentSections: DropdownOption[];
+    fetchStudents: () => Promise<void>;
 }
 const StudentsContext = createContext<StudentsContextType | undefined>(undefined)
 
@@ -18,8 +18,9 @@ const useStudentsProvider = (): StudentsContextType => {
     const [displayStudents, setDisplayStudents] = useState<Student[]>([])
     const [sections, setSections] = useState<DropdownOption[]>([])
     const fetchStudents = async () => {
-        const fixedCourseNum = 1  // to-do: replace with actual course number
-        const courseMemberData = await fetch(`http://127.0.0.1:8000/api/v1/course-members/?course=${fixedCourseNum}`).then(res => res.json())
+        const fixedCourseNum = 1  // TODO: replace with actual course number
+        const baseURL = process.env.NEXT_PUBLIC_DB_HOST
+        const courseMemberData = await fetch(`${baseURL}/api/v1/course-members/course/${fixedCourseNum}/`).then(res => res.json())
         const studentsToDisplay: Student[] = courseMemberData.results.map((member: any) => ({
             id: member.id,
             name: member.user.last_name + ',' + member.user.first_name,
@@ -28,7 +29,7 @@ const useStudentsProvider = (): StudentsContextType => {
         setDisplayStudents(studentsToDisplay)
     }
 
-    // to-do: remove this useMemo and use API call to get a set of sections all student course_members that are in specific course
+    // TODO: remove this useMemo and use API call to get a set of sections all student course_members that are in specific course
     useMemo(() => {
         const sections = new Set<string>()
         displayStudents.forEach(student => {
