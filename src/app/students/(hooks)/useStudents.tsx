cf39f87b6cbe4b141589068a3e762204ab3166a6
Abhinav Thota <59943893+abeeto@users.noreply.github.com>
2024-03-24 09:55:51 -0700

    Set up server side pagination (#36)
    * updated students page UI to no longer have CSV upload
    
    * removed file upload hooks
    
    * updated useStudents hook to no longer be updated by file changes
    
    * removed filehandling logic and added api call
    
    * refactored fetchStudents function for pagination
    
    * made student-table-pagination for server side pagination
    
    * add relevant logic for serverside pagination
    
    * added minor documentation and moved fetchStudents function
    
    * add minor documentation
    
    * remove parseCSV.ts file
    
    * remove parseCSV.ts
    
    * using react table pagination internal state. removed students-table-pagination.tsx
    
    * removed dead code in student-table-toolbar.tsx
    
    * use env variable instead
    
    * change local env to next_public_backend_url

--- src/app/students/(hooks)/useStudents.tsx
+++ src/app/students/(hooks)/useStudents.tsx
@@ -1,38 +1,65 @@
 'use client'
-import { useState, createContext, useContext, ChangeEvent, PropsWithChildren, useMemo } from 'react'
+import { useState, createContext, useContext, PropsWithChildren, useMemo, useEffect } from 'react'
 import { Student } from '@/_temp_types/student'
+import { useSearchParams } from 'next/navigation'
 
-type DropdownOption  = {
-  label: string;
-  value: string;
+type DropdownOption = {
+    label: string;
+    value: string;
 }
 
 type StudentsContextType = {
   displayStudents: Student[];
   currentSections: DropdownOption[];
+  totalStudents: number;
 }
-
 const StudentsContext = createContext<StudentsContextType | undefined>(undefined)
 
 const useStudentsProvider = (): StudentsContextType => {
-    const [studentsParse, setStudentsParse] = useState<Student[]>([])
     const [displayStudents, setDisplayStudents] = useState<Student[]>([])
     const [sections, setSections] = useState<DropdownOption[]>([])
+    const [totalStudents, setTotalStudents] = useState<number>(0)
+    const searchParams = useSearchParams()
 
+
+    const fixedCourseNum = 1
+
+    const { pageIndex, pageSize } = useMemo(() => {
+        return {
+            pageIndex: parseInt(searchParams.get('page') ?? '1'),
+            pageSize: parseInt(searchParams.get('per_page') ?? '10'),
+        }
+    }, [searchParams])
+
+    useEffect(() => {
+        const fetchStudents = async () => {
+            const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL
+            const courseMemberData = await fetch(`${baseURL}/api/v1/course-members/course/${fixedCourseNum}/?page=${pageIndex}&per_page=${pageSize}`).then(res => res.json())
+            const studentsToDisplay: Student[] = courseMemberData.results.map((member: any) => ({
+                id: member.id,
+                name: member.user.last_name + ',' + member.user.first_name,
+                sections: member.sections.map((section: any) => section.name),
+            }))
+            setDisplayStudents(studentsToDisplay)
+            setTotalStudents(courseMemberData.count)
+        }
+        fetchStudents()
+    }, [pageIndex, pageSize])
     useMemo(() => {
         const sections = new Set<string>()
-        studentsParse.forEach(student => {
+        displayStudents.forEach(student => {
             student.sections?.forEach(section => {
                 sections.add(section)
             })
         })
         const sectionsOptions: DropdownOption[] = Array.from(sections).map(section => ({ label: section, value: section }))
         setSections(sectionsOptions)
-    }, [studentsParse])
+    }, [displayStudents])
 
     return {
         displayStudents: displayStudents,
         currentSections: sections,
+        totalStudents: totalStudents,
     }
 }
 
