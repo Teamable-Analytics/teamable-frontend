8aab055b678a6036d9a0f302821f3f946eb8c4a2
ketphan02 <tuankietvn@outlook.com>
2024-03-24 19:29:35 -0700

    Resolve comments

--- src/_temp_types/api/teams.ts
+++ src/_temp_types/api/teams.ts
@@ -1,28 +1,22 @@
-export type ApiProjectSet = {
+export type ApiTeamSetTemplate = {
     id: number
     name: string
-    teams: ApiProject[]
+    teams: ApiTeamTemplate[]
     createdAt: string
     updatedAt: string
 }
 
-export type ApiProject = {
+export type ApiTeamTemplate = {
     id: number
     name: string
-    members: ApiCourseMember[] // This is supposed to be empty
     createdAt: string
     updatedAt: string
     slug: string
     requirements: ApiProjectRequirement[]
     max_people: number
     min_people: number
-    team_set: number
 }
 
 export type ApiProjectRequirement = {
-    // TODO: Update this
-}
-
-export type ApiCourseMember = {
     // TODO: Update this
 }
--- src/app/project-sets/page.tsx
+++ src/app/project-sets/page.tsx
@@ -2,17 +2,17 @@ import React from "react"
 import {columns} from "@/app/project-sets/columns"
 import {DataTable} from "@/components/ui/data-table"
 import PageView from "@/components/views/Page"
-import {type ApiProjectSet} from "@/_temp_types/api/teams"
+import {type ApiTeamSetTemplate} from "@/_temp_types/api/teams"
 import {type ProjectSet} from "@/_temp_types/projectSet"
 import {redirect} from "next/navigation"
 
 async function getProjectSetsData(): Promise<ProjectSet[]> {
-    const response = await fetch(process.env.BACKEND_BASE_URI + '/api/v1/teamset-templates')
+    const response = await fetch(process.env.BACKEND_URL + '/api/v1/teamset-templates')
     if (!response.ok) {
         throw new Error('Unable to fetch project sets from API.')
     }
     const teamSets = await response.json()
-    return teamSets.map(({id, name, teams}: ApiProjectSet) => ({
+    return teamSets.map(({id, name, teams}: ApiTeamSetTemplate) => ({
         id,
         name,
         numProjects: teams.length,
