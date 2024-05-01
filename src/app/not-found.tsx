00617954295eebe42879930a8d3394c8ff7fcc1e
SanaBob <minhquanle_2002@yahoo.com>
2024-05-01 09:27:41 -0700

    custom 404 page (#43)
    * custom 404 page
    
    * fixed ui for 404 + redirection
    
    * fixed spacing and component style import

---
+++ src/app/not-found.tsx
@@ -0,0 +1,18 @@
+import { Text } from "@/components/ui/text"
+
+const Custom404 = ({ errorMessage }: { errorMessage: string }) => {
+  return (
+    <div className="flex h-screen">
+      <div className="m-auto flex flex-col items-center">
+        <Text element="h3" as="h3">
+          Page Not Found
+        </Text>
+        <Text element="p" as="smallText">
+          { errorMessage }
+        </Text>
+      </div>
+    </div>
+  )
+}
+
+export default Custom404
