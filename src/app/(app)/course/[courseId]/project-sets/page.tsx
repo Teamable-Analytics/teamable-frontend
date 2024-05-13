cd4b02494c816229c6526d8d2fec12e3fa668581
Kiet (K) Phan <35695324+ketphan02@users.noreply.github.com>
2024-05-13 18:51:08 -0400

    Project Page Detail Part 3 (#48)

--- src/app/(app)/course/[courseId]/project-sets/page.tsx
+++ src/app/(app)/course/[courseId]/project-sets/page.tsx
@@ -1,37 +1,37 @@
 import React from "react"
-import { DataTable } from "@/components/ui/data-table"
+import {DataTable} from "@/components/ui/data-table"
 import PageView from "@/components/views/Page"
-import { type ApiTeamSetTemplate } from "@/_temp_types/api/teams"
-import { type ProjectSet } from "@/_temp_types/projectSet"
-import { redirect } from "next/navigation"
-import { columns } from "./columns"
+import {type ApiTeamSetTemplate} from "@/_temp_types/api/teams"
+import {type ProjectSet} from "@/_temp_types/projectSet"
+import {redirect} from "next/navigation"
+import {columns} from "./columns"
 
-async function getProjectSetsData(): Promise<ProjectSet[]> {
+const getProjectSetsData = async (): Promise<ProjectSet[]> => {
   const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + "/api/v1/teamset-templates")
   if (!response.ok) {
     throw new Error("Unable to fetch project sets from API.")
   }
   const teamSets = await response.json()
-  return teamSets.map(({ id, name, teams }: ApiTeamSetTemplate) =>
-      ({
-        id,
-        name,
-        numProjects: teams.length,
-      }) as ProjectSet,)
+  return teamSets.map(({id, name, teams}: ApiTeamSetTemplate) =>
+    ({
+      id,
+      name,
+      numProjects: teams.length,
+    }) as ProjectSet,)
 }
 
 async function ProjectSetsPage() {
   const handleRowClick = async (row: ProjectSet) => {
     "use server"
-    redirect(`/project-sets/${row.id}`)
+    redirect(`project-sets/${row.id}`)
   }
 
   return (
     <PageView
       title="Project Sets"
       breadcrumbs={[
-        { title: "Home", href: "/" },
-        { title: "Project Sets", href: "/project-sets" },
+        {title: "Home", href: "/"},
+        {title: "Project Sets", href: `/project-sets`},
       ]}
     >
       <DataTable<ProjectSet>
