f50ac77b8daf292a43d63960cfb52d74db48afdb
ketphan02 <tuankietvn@outlook.com>
2024-05-12 22:52:23 -0700

    lint

--- src/app/(app)/course/[courseId]/project-sets/page.tsx
+++ src/app/(app)/course/[courseId]/project-sets/page.tsx
@@ -1,10 +1,10 @@
 import React from "react"
-import { DataTable } from "@/components/ui/data-table"
+import {DataTable} from "@/components/ui/data-table"
 import PageView from "@/components/views/Page"
-import { type ApiTeamSetTemplate } from "@/_temp_types/api/teams"
-import { type ProjectSet } from "@/_temp_types/projectSet"
-import { redirect } from "next/navigation"
-import { columns } from "./columns"
+import {type ApiTeamSetTemplate} from "@/_temp_types/api/teams"
+import {type ProjectSet} from "@/_temp_types/projectSet"
+import {redirect} from "next/navigation"
+import {columns} from "./columns"
 
 async function getProjectSetsData(): Promise<ProjectSet[]> {
   const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + "/api/v1/teamset-templates")
@@ -12,12 +12,12 @@ async function getProjectSetsData(): Promise<ProjectSet[]> {
     throw new Error("Unable to fetch project sets from API.")
   }
   const teamSets = await response.json()
-  return teamSets.map(({ id, name, teams }: ApiTeamSetTemplate) =>
-      ({
-        id,
-        name,
-        numProjects: teams.length,
-      }) as ProjectSet,)
+  return teamSets.map(({id, name, teams}: ApiTeamSetTemplate) =>
+    ({
+      id,
+      name,
+      numProjects: teams.length,
+    }) as ProjectSet)
 }
 
 async function ProjectSetsPage() {
@@ -30,8 +30,8 @@ async function ProjectSetsPage() {
     <PageView
       title="Project Sets"
       breadcrumbs={[
-        { title: "Home", href: "/" },
-        { title: "Project Sets", href: "/project-sets" },
+        {title: "Home", href: "/"},
+        {title: "Project Sets", href: "/project-sets"},
       ]}
     >
       <DataTable<ProjectSet>
