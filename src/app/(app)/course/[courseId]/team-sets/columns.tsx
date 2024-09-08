5442727dc5788f3fa0224ba23d9ae0f0b0b0db72
opeyem1a <opeyadeyemi@gmail.com>
2024-09-08 03:42:38 -0600

    remove dropdown menu from teamsets table

--- src/app/(app)/course/[courseId]/team-sets/columns.tsx
+++ src/app/(app)/course/[courseId]/team-sets/columns.tsx
@@ -1,12 +1,16 @@
 import { ColumnDef } from "@tanstack/table-core"
 import { DataTableColumnHeader } from "@/components/ui/data-table-column-header"
-import {TeamSet} from "@/_temp_types/teamSet"
+import { TeamSet } from "@/_temp_types/teamSet"
 
 export const columns: ColumnDef<Omit<TeamSet, "teams">>[] = [
   {
     accessorKey: "name",
     header: ({ column }) => (
-      <DataTableColumnHeader column={column} title={"Name"} />
+      <DataTableColumnHeader
+        column={column}
+        title={"Name"}
+        hasDropDownMenu={false}
+      />
     ),
   },
 ]
