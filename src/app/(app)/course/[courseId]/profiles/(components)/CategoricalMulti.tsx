b0e305d43369fb313696edd762129323cdc2b740
Colton Palfrey <101154480+colton456p@users.noreply.github.com>
2024-10-10 16:07:36 -0700

    Updated files using downloaded svg's for icons and replaced them with imported Radix icons. (#87)

--- src/app/(app)/course/[courseId]/profiles/(components)/CategoricalMulti.tsx
+++ src/app/(app)/course/[courseId]/profiles/(components)/CategoricalMulti.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { Checkbox } from "@/components/ui/checkbox"
 import { Button } from "@/components/ui/button"
 import {
   Card,
@@ -10,19 +9,19 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { Checkbox } from "@/components/ui/checkbox"
+import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { DndContext, DragEndEvent, closestCenter } from "@dnd-kit/core"
 import {
   SortableContext,
-  verticalListSortingStrategy,
-  useSortable,
   arrayMove,
+  useSortable,
+  verticalListSortingStrategy,
 } from "@dnd-kit/sortable"
-import { DndContext, DragEndEvent, closestCenter } from "@dnd-kit/core"
 import { CSS } from "@dnd-kit/utilities"
-import { Formik, Form, useFormikContext } from "formik"
-import Trash from "@public/trash-icon.svg"
-import Drag from "@public/drag-icon.svg"
-import { Input } from "@/components/ui/input"
+import { Form, Formik, useFormikContext } from "formik"
+import { DragHandleDots2Icon, TrashIcon } from '@radix-ui/react-icons'
 
 type Answer = {
   id: number;
@@ -63,7 +62,7 @@ const SortableInput = ({ index }: { index: number }) => {
                   values.answers.filter((_, i: number) => i !== index),)
               }
             >
-              <Trash />
+              <TrashIcon />
             </Button>
             <button
               type="button"
@@ -71,7 +70,7 @@ const SortableInput = ({ index }: { index: number }) => {
               ref={setNodeRef}
               {...listeners}
             >
-              <Drag />
+              <DragHandleDots2Icon />
             </button>
           </>
         ) : (
