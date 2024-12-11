4d3d4d8233047f13667e828c0ecca7b609575167
colton456p <colton.p@hotmail.com>
2024-12-11 12:24:45 -0800

    Added a conditional to limit the number of attributes that will be shown on the homepage.

--- src/app/(app)/course/[courseId]/home/(components)/AttributesUsed.tsx
+++ src/app/(app)/course/[courseId]/home/(components)/AttributesUsed.tsx
@@ -1,25 +1,37 @@
 import { Attribute } from "@/_temp_types/homeAttributes"
 
-const AttributesUsed = ({ attributes }: { attributes: Attribute[] }) => (
-  <div className="mb-8">
-    <h3 className="text-lg font-semibold mb-2">Attributes Used</h3>
-    <div className="border border-gray-300 p-4 rounded-md">
-      <div className={`grid gap-2 ${attributes.length > 0 ? "grid-cols-1 sm:grid-cols-4" : ""}`}>
-        {attributes.length > 0 ? (
-          attributes.map((attribute, index) => (
-            <ul key={index} className="list-disc pl-5">
-              <li>{attribute.name}</li>
+const AttributesUsed = ({ attributes }: { attributes: Attribute[] }) => {
+  const displayedAttributes = attributes.slice(0, 15)
+  const hasMoreAttributes = attributes.length > 15
+
+  return (
+    <div className="mb-8">
+      <h3 className="text-lg font-semibold mb-2">Attributes Used</h3>
+      <div className="border border-gray-300 p-4 rounded-md">
+        <div className={`grid gap-2 ${attributes.length > 0 ? "grid-cols-1 sm:grid-cols-4" : ""}`}>
+          {attributes.length > 0 ? (
+            <>
+              {displayedAttributes.map((attribute, index) => (
+                <ul key={index} className="list-disc pl-5">
+                  <li>{attribute.name}</li>
+                </ul>
+              ))}
+              {hasMoreAttributes && (
+                <ul className="list-disc pl-5">
+                  <li>...</li>
+                </ul>
+              )}
+            </>
+          ) : (
+            <ul className="list-disc pl-5">
+              <li>No attributes used</li>
             </ul>
-          ))
-        ) : (
-          <ul className="list-disc pl-5">
-            <li>No attributes used</li>
-          </ul>
-        )}
+          )}
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 AttributesUsed.defaultProps = {
   attributes: [],
