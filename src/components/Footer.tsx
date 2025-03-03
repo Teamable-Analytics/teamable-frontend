0b40430d369750a7117f50cf55c7fc17bdc80085
Opey Adeyemi <60050089+Opeyem1a@users.noreply.github.com>
2025-03-03 08:15:26 -0700

    chore: update footer year (#94)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="flex justify-between items-center px-16 py-8">
       <Text element="p" as="smallText" className="p-0">
-        Teamable, 2024.
+        Teamable, 2025.
       </Text>
       <Text element="p" as="smallText" className="p-0 mt-0">
         ✨
